dccd9a263349251a90ab75ee8aaf917bf1f1e2d5
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-03-29 00:03:24 -0700

    Need to create the checker for letters in use

--- player.rb
+++ player.rb
@@ -1,1 +1,9 @@
-class Player;end
+class Player
+  attr_reader :letter
+
+  def initialize(input_letter = nil) 
+    raise ArgumentError.new("Requires character input") if input_letter.nil?
+    letter = input_letter.upcase.to_sym
+    @letter = letter
+  end
+end
